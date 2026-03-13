b591fb6c4d4781adae1b509243411a35538bb85d
stefpi <19478336+stefpi@users.noreply.github.com>
2026-03-12 20:58:15 -0700

    feat: 10mbps bitrate for connect webrtc livestream

--- system/loggerd/loggerd.h
+++ system/loggerd/loggerd.h
@@ -47,7 +47,7 @@ struct EncoderSettings {
   }
 
   static EncoderSettings StreamEncoderSettings() {
-    int _stream_bitrate = getenv("STREAM_BITRATE") ? atoi(getenv("STREAM_BITRATE")) : 1'000'000;
+    int _stream_bitrate = getenv("STREAM_BITRATE") ? atoi(getenv("STREAM_BITRATE")) : 10'000'000;
     return EncoderSettings{.encode_type = cereal::EncodeIndex::Type::QCAMERA_H264, .bitrate = _stream_bitrate , .gop_size = 15};
   }
 };
