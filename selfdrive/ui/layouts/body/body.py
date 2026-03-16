dd13a9b3d9fcf02340c731b8fc8fc85d8f6d079e
stefpi <19478336+stefpi@users.noreply.github.com>
2026-03-16 09:52:44 -0700

    fix: remove wink all the time

--- selfdrive/ui/layouts/body/body.py
+++ selfdrive/ui/layouts/body/body.py
@@ -162,9 +162,9 @@ class BodyLayout(Widget):
         self._last_input_time = time.monotonic()
 
       if time.monotonic() - self._last_input_time > IDLE_TIMEOUT:
-        self._animator.set_animation(WINK)
+        self._animator.set_animation(INQUISITIVE)
       else:
-        self._animator.set_animation(WINK)
+        self._animator.set_animation(NORMAL)
     else:
       self._was_active = False
       self._animator.set_animation(ASLEEP)
